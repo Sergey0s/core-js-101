879a2a258fbfe40146933c86b4e9eb20a99934f5
Sergey0s <mediaworld@mail.ru>
2020-05-17 20:24:59 +0300

    feat: complete promises task

--- src/07-promises-tasks.js
+++ src/07-promises-tasks.js
@@ -28,8 +28,20 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    let answer = '';
+    if (isPositiveAnswer === true) {
+      answer = 'Hooray!!! She said "Yes"!';
+      resolve(answer);
+    } else if (isPositiveAnswer === false) {
+      answer = 'Oh no, she said "No".';
+      resolve(answer);
+    } else {
+      answer = new Error('Wrong parameter is passed! Ask her again.');
+      reject(answer);
+    }
+  });
 }
 
 
@@ -48,8 +60,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -71,8 +83,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
